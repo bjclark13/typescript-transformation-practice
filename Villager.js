8d14a58a3f96ca5a30f98515af4fa6ba0a9e3090
BJ Clark <bjclark13@outlook.com>
2020-12-17 21:29:48 -0500

    Solution

--- Villager.js
+++ Villager.js
@@ -1,35 +1,36 @@
+"use strict";
 class Villager {
-  constructor(name, birthday, catchPhrase) {
-    this.name = name;
-    this.birthday = birthday;
-    this.catchPhrase = catchPhrase;
-    this.pockets = [];
-    this.tools = {
-      hammer: {
-        health: 2,
-        type: "basic",
-      },
-    };
-  }
-
-  sayHello() {
-    return `Hello, my name is ${this.name} and I was born on ${this.birthday}. ${this.catchPhrase}!`;
-  }
-
-  addToPockets(item) {
-      this.pockets.push(item);
-  }
-
-  useTool(toolName) {
-    if (this.tools[toolName]) {
-      const tool = this.tools[toolName];
-      tool.health--;
-      if (tool.health <= 0) {
-        // the tool broke :()
-        console.log(`Oh no! That's it for my trusty ${tool.type} ${toolName}`);
-        // remove from object
-        delete this.tools[toolName];
-      }
+    constructor(name, birthday, catchPhrase) {
+        this.name = name;
+        // can either put public, or declare
+        this.birthday = new Date(birthday);
+        this.catchPhrase = catchPhrase;
+        this.pockets = [];
+        this.tools = {
+            hammer: {
+                health: 2,
+                type: 'basic',
+            },
+        };
     }
-  }
+    sayHello() {
+        return `Hello, my name is ${this.name} and I was born on ${this.birthday}. ${this.catchPhrase}!`;
+    }
+    addToPockets(item) {
+        this.pockets.push(item);
+    }
+    useTool(toolName) {
+        if (this.tools[toolName]) {
+            const tool = this.tools[toolName];
+            tool.health--;
+            if (tool.health <= 0) {
+                // the tool broke :()
+                console.log(`Oh no! That's it for my trusty ${tool.type} ${toolName}`);
+                // remove from object
+                delete this.tools[toolName];
+            }
+        }
+    }
 }
+const villager = new Villager("BJ", '09-15-1900', "Hey there!");
+villager.useTool('hammer');
---
+++ Villager.ts
@@ -0,0 +1,51 @@
+interface Tool {
+  health: number,
+  type: string
+}
+
+class Villager {
+	birthday: Date; // object , string would be 12-20-20
+	catchPhrase: string;
+	pockets: any[];
+	tools: any;
+
+	constructor(public name: string, birthday: string, catchPhrase: string) {
+		// can either put public, or declare
+		this.birthday = new Date(birthday);
+		this.catchPhrase = catchPhrase;
+		this.pockets = [];
+		this.tools = {
+			hammer: {
+				health: 2,
+				type: 'basic',
+			},
+		};
+	}
+
+	sayHello(): string {
+		return `Hello, my name is ${this.name} and I was born on ${this.birthday}. ${this.catchPhrase}!`;
+	}
+
+	addToPockets(item: any) {
+		this.pockets.push(item);
+	}
+
+	useTool(toolName: string) {
+		if (this.tools[toolName]) {
+			const tool: Tool = this.tools[toolName];
+			tool.health--;
+			if (tool.health <= 0) {
+				// the tool broke :()
+				console.log(`Oh no! That's it for my trusty ${tool.type} ${toolName}`);
+				// remove from object
+				delete this.tools[toolName];
+			}
+		}
+	}
+}
+
+const villager = new Villager("BJ", '09-15-1900', "Hey there!");
+villager.useTool('hammer');
+
+const villagerElement = document.querySelector('#villager');
+villagerElement?.innerHTML = villager.sayHello();
